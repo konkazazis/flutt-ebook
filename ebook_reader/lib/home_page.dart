18ff6c835297ceef57b89c7af74916162c383928
konkazazisa <nikos_kazazis@yahoo.com>
2023-08-23 00:26:30 +0300

    added bottom menu  and dummy pages

--- ebook_reader/lib/home_page.dart
+++ ebook_reader/lib/home_page.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'bookmarks_page.dart';
+import 'bookshelf_page.dart';
+import 'music_page.dart';
+import 'settings_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    BookmarksPage(),
+    BookshelfPage(),
+    MusicPage(),
+    SettingsPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
-      body: Center(
-        child: Text('Welcome to the Ebook Reader App!'),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: 'Bookmarks'),
+          BottomNavigationBarItem(icon: Icon(Icons.library_books), label: 'Bookshelf'),
+          BottomNavigationBarItem(icon: Icon(Icons.music_note), label: 'Music'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+        ],
       ),
     );
   }
