cc896d6dcbd22be580178b585eb85c96dde0fe37
Konstantinos Kazazis <kkazazis@softone.gr>
2023-08-24 10:48:08 +0300

    settings page

--- ebook_reader/lib/settings_page.dart
+++ ebook_reader/lib/settings_page.dart
@@ -3,8 +3,53 @@ import 'package:flutter/material.dart';
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Settings Page'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/profile_picture.jpg'),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'John Doe',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text('john.doe@example.com',
+                style: TextStyle(fontSize: 16, color: Colors.grey)),
+            SizedBox(height: 24),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('Notifications'),
+              trailing: Switch(
+                value: true, // Replace with your logic for the switch
+                onChanged: (value) {
+                  // Handle switch changes
+                },
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.language),
+              title: Text('Language'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                // Navigate to the language settings page
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Logout'),
+              onTap: () {
+                // Handle user logout
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
