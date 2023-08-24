7b244533a79b3afc9c577f3ecaa12b88349c20f6
Konstantinos Kazazis <kkazazis@softone.gr>
2023-08-24 14:56:24 +0300

    dummy data

--- ebook_reader/lib/bookmarks_page.dart
+++ ebook_reader/lib/bookmarks_page.dart
@@ -3,8 +3,23 @@ import 'package:flutter/material.dart';
 class BookmarksPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Bookmarks Page'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Bookmarks')),
+      body: ListView.builder(
+        itemCount: 5, // Number of dummy bookmarks
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text('Bookmark Title ${index + 1}'),
+              subtitle: Text(
+                  'This is a small passage of text that represents a bookmark.'),
+              onTap: () {
+                // Implement bookmark selection and navigation here
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
