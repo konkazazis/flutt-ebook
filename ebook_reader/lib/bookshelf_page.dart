13f2a05288a7e182e7ef6ae345464b83acc04985
Konstantinos Kazazis <kkazazis@softone.gr>
2023-08-24 14:05:02 +0300

    added templates for music,bookshelf

--- ebook_reader/lib/bookshelf_page.dart
+++ ebook_reader/lib/bookshelf_page.dart
@@ -3,8 +3,30 @@ import 'package:flutter/material.dart';
 class BookshelfPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Bookshelf Page'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Bookshelf')),
+      body: GridView.builder(
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: 6, // Number of dummy ebooks
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              // Implement ebook opening here
+            },
+            child: Card(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/book_cover.jpg', height: 100, width: 80),
+                  SizedBox(height: 8),
+                  Text('Ebook Title ${index + 1}'),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- ebook_reader/lib/music_page.dart
+++ ebook_reader/lib/music_page.dart
@@ -3,8 +3,22 @@ import 'package:flutter/material.dart';
 class MusicPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Music Page'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Music')),
+      body: ListView.builder(
+        itemCount: 5, // Number of dummy songs
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.music_note),
+            title: Text('Song Title ${index + 1}'),
+            subtitle: Text('Artist ${index + 1}'),
+            trailing: Text('3:30'),
+            onTap: () {
+              // Implement song playback here
+            },
+          );
+        },
+      ),
     );
   }
 }
